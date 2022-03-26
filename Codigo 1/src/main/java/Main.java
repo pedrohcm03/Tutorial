0131b4c47054da019ffc61b297094f92cf5ee66a
pedrohcm03 <pedrohcmiranda@get.inatel.br>
2022-03-26 16:49:00 -0300

    Adicionei mais um print

--- Codigo 1/src/main/java/Main.java
+++ Codigo 1/src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Tutorial de git");
+        System.out.println("Adicionando mais uma linha!");
     }
 }
